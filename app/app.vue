4b66cbf58f27c58a97836aa3e0e11580709eefd2
Hafiz Javaid Iqbal <hafizjaidi786@gmail.com>
2025-12-23 11:45:03 +0500

    feat: bootstrap setup

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button type="button" class="btn btn-primary">Primary</button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon']
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css',
+    './app/assets/main.scss',
+  ],
+  app: {
+    head: {
+      script: [
+        {
+          src: 'bootstrap/dist/js/bootstrap.bundle.min.js',
+          tagPosition: 'bodyClose'
+        }
+      ]
+    }
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
